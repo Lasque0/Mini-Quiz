1d0de147af629f972e7f3d7380d278265b70e6ce
Lasque <lasquee0@gmail.com>
2025-09-04 01:54:24 +0300

    Add files via upload

---
+++ Mini Quiz.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    double x1,x2,x3,x4,x5,x6;
+
+    cout<<"50+50= ";
+    cin>>x1;
+
+    if(x1==100)
+        cout<<"True!";
+    else if(x1==98)
+        cout<<"You came so close";
+    else if(x1==99)
+        cout<<"You came so close";
+    else if(x1==101)
+        cout<<"You came so close";
+    else if(x1==102)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+    cout<<"\n\n\n";
+
+
+    cout<<"252+150= ";
+    cin>>x2;
+
+    if(x2==402)
+        cout<<"True!";
+    else if(x2==400)
+        cout<<"You came so close";
+    else if(x2==401)
+        cout<<"You came so close";
+    else if(x2==403)
+        cout<<"You came so close";
+    else if(x2==404)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+    cout<<"\n\n\n";
+
+
+    cout<<"575+935= ";
+    cin>>x3;
+
+    if(x3==1510)
+        cout<<"True!";
+    else if(x3==1508)
+        cout<<"You came so close";
+    else if(x3==1509)
+        cout<<"You came so close";
+    else if(x3==1511)
+        cout<<"You came so close";
+    else if(x3==1512)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+    cout<<"\n\n\n";
+
+
+    cout<<"1405+2505= ";
+    cin>>x4;
+
+    if(x4==3909)
+        cout<<"True!";
+    else if(x4==3907)
+        cout<<"You came so close";
+    else if(x4==3908)
+        cout<<"You came so close";
+    else if(x4==3910)
+        cout<<"You came so close";
+    else if(x4==3911)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+
+    cout<<"\n\n\n";
+
+
+
+    cout<<"3500+3600+3700= ";
+    cin>>x5;
+
+    if(x5==10800)
+        cout<<"True!";
+    else if(x5==10798)
+        cout<<"You came so close";
+    else if(x5==10799)
+        cout<<"You came so close";
+    else if(x5==10801)
+        cout<<"You came so close";
+    else if(x5==10802)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+
+    cout<<"\n\n\n";
+
+
+
+    cout<<"500/25= ";
+    cin>>x6;
+
+    if(x6==25)
+        cout<<"True!";
+    else if(x6==23)
+        cout<<"You came so close";
+    else if(x6==24)
+        cout<<"You came so close";
+    else if(x6==26)
+        cout<<"You came so close";
+    else if(x6==27)
+        cout<<"You came so close";
+    else
+        cout<<"False";
+
+
+    return 0;
+}
